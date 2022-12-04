747bc7a9a5a73d8cba4b27264d86ded614eef3e9
KenyOgun <holleraturboi@yahoo.ca>
2022-12-03 20:44:53 -0500

    testing logout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Login from "./user/Login";
+import Logout from "./user/Logout";
 
 export default function App() {
-  return <Login />;
+  return <Logout user="Kehinde Ogunnigbo" />;
 }
