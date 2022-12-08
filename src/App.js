1e94149c188f05af061fdc49ffbee8f4cefb2686
KenyOgun <holleraturboi@yahoo.ca>
2022-12-07 23:54:58 -0500

    testing PostList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-import CreatePost from "./post/CreatePost";
+import PostList from "./post/PostList";
+
 
 export default function App() {
   return <CreatePost />;
+}
+const posts = [
+ { title: 'React Hooks', content: 'The greatest thing since
+sliced bread!', author: 'Kehinde Ogunnigbo' },
+ { title: 'Using React Fragments', content: 'Keeping the DOM
+tree clean!', author: 'Kehinde Ogunnigbo' }
+]
+export default function App () {
+ return <PostList posts={posts} />
 }
